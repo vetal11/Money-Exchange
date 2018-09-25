3293ee7426ec06899d2701c8eb88c057595c80d7
vetal11 <vetalblack@list.ru>
2018-09-25 23:06:15 +0300

    !

--- src/index.js
+++ src/index.js
@@ -1,5 +1,29 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+     
+	let coins = [50,  25,   10,  5,   1 ];
+	let coinsCode = ['H', 'Q', 'D', 'N', 'P'];
+	let result = {};
+    
+
+	if (currency > 10000) {
+		return  { error : "You are rich, my friend! We don\'t have so much coins for exchange" };
+		
+    }
+    
+	if (currency <= 0) {
+        return {};
+    }
+
+    for(let i=0; i<=4; i++){
+      let b = coins[i];
+      let c = coinsCode[i];    
+
+      while(Math.floor(currency/b) >= 1){ 
+        result[c] = Math.floor(currency/b)
+        currency = currency - b*result[c];
+      }
+    }
+    return result;
+
 }
